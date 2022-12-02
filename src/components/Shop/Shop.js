ca5cf80c4579c8ea0a0093c7629a820df2570cf1
Munna <minhaj.munnaa@gmail.com>
2022-12-02 03:44:16 +0000

    third commit

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { addToDb, getStoredCart } from '../../utilities/fakedb';
 import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import './Shop.css';
@@ -14,10 +15,39 @@ const Shop = () => {
         .then(data=> setProducts(data))
     },[]);
 
-    const handleAddToCart = (product) =>{
-        console.log(product);
-        const newCart = [...cart, product];
+
+    useEffect(() =>{
+        console.log('Local storage',products)
+        const storedCart = getStoredCart();
+        const savedCart =[];
+        for(const id in storedCart){
+            const addedProduct = products.find(product=> product.id === id);
+            if(addedProduct){
+                const quantity = storedCart[id];
+                addedProduct.quantity = quantity;
+                savedCart.push(addedProduct);
+            }
+            console.log(addedProduct);
+        }
+        setCart(savedCart);
+    },[products])
+
+    const handleAddToCart = (selectedProduct) =>{
+        console.log(selectedProduct);
+        let newCart=[];
+        const exists = cart.find(product => product.id === selectedProduct.id);
+        if(!exists){
+           selectedProduct.quantity = 1;
+           newCart = [...cart,selectedProduct];
+        }
+        else{
+            const rest = cart.filter(product => product.id !==selectedProduct.id);
+            exists.quantity = exists.quantity+1;
+            newCart = [...rest,exists];
+        }
         setCart(newCart);
+        addToDb(selectedProduct.id);
+
     }
     return (
         <div className='Shop-container'>
--- src/utilities/fakedb.js
+++ src/utilities/fakedb.js
@@ -19,6 +19,16 @@ const addToDb = id =>{
     }
     localStorage.setItem('shopping-cart', JSON.stringify(shoppingCart));
 }
+const getStoredCart = ()=>{
+    let shoppingCart = {};
+
+    //get the shopping cart from local storage
+    const storedCart = localStorage.getItem('shopping-cart');
+    if(storedCart){
+        shoppingCart = JSON.parse(storedCart);
+    }
+return shoppingCart;
+}
 
 const removeFromDb = id =>{
     const storedCart = localStorage.getItem('shopping-cart');
@@ -37,6 +47,7 @@ const deleteShoppingCart = () =>{
 
 export {
     addToDb, 
+    getStoredCart,
     removeFromDb,
     deleteShoppingCart
 }
